a8f00a2b322465527a62e3db17ad8e9721f861a6
Simon Bojesen <simon@schonberg.dk>
2019-05-15 14:21:50 +0200

    login

---
+++ fetch/UserFetch.js
@@ -0,0 +1,69 @@
+import jwt_decode from 'jwt-decode';
+import { AsyncStorage } from 'react-native';
+const URL = "https://rasmuslynge.com/jwtbackend";
+
+function handleHttpErrors(res) {
+    if (!res.ok) {
+        return { status: res.status, fullError: res.json() };
+    }
+    return res.json();
+}
+
+class UserFetch {
+
+    fetchData = async () => {
+        const token = await this.getToken();
+        console.log("token", token);
+        var decoded = jwt_decode(token);
+        const options = await this.makeOptions("GET", true); //True add's the token
+        return fetch(URL + "/api/info/" + decoded.username, options).then(handleHttpErrors);
+    };
+
+    login = async (user, pass) => {
+        const options = await this.makeOptions("POST", true, {
+            username: user,
+            password: pass
+        });
+        const result = await fetch(URL + "/api/login", options)
+            .then(handleHttpErrors);
+            console.log("result", result);
+        this.setToken(result.token);
+        
+    };
+
+    setToken = async token => {
+        await AsyncStorage.setItem("jwtToken", token);
+    };
+
+    getToken = () => {
+        return AsyncStorage.getItem("jwtToken");
+    };
+
+    loggedIn = () => {
+        const loggedIn = this.getToken() != null;
+        return loggedIn;
+    };
+
+    logout = async () => {
+        await AsyncStorage.removeItem("jwtToken");
+    };
+
+    async makeOptions(method, addToken, body) {
+        var opts = {
+            method: method,
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json"
+            }
+        };
+        if (addToken && this.loggedIn()) {
+            opts.headers["x-access-token"] = await this.getToken();
+        }
+        if (body) {
+            opts.body = JSON.stringify(body);
+        }
+        return opts;
+    }
+}
+const facade = new UserFetch();
+export default facade;
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View, Image, Platform } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Image, Platform, TextInput, Button } from "react-native";
 import { ExpoLinksView } from "@expo/samples";
-
+import Facade from "../fetch/UserFetch";
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     header: null
+  };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  login = async evt => {
+    evt.preventDefault();
+    await Facade.login(this.state.username, this.state.password)
+    this.props.navigation.navigate('Home');
+    //console.log(this.state.username, this.state.password)
   };
 
   render() {
@@ -17,7 +32,23 @@ export default class LinksScreen extends React.Component {
           />
         </View>
         <ScrollView style={styles.container}>
-          <Text>Login could be here</Text>
+        <Text>Login Screen</Text>
+        <TextInput
+            placeholder="Enter Username"
+            style={{ width: 200, margin: 10 }}
+            onChangeText={username => this.setState({ username })}
+        />
+
+        <TextInput
+            placeholder="Enter Password"
+            style={{ width: 200, margin: 10 }}
+            onChangeText={password => this.setState({ password })}
+        />
+        <Text>{JSON.stringify(this.state)}</Text>
+        <Button
+            title="Submit"
+            onPress={ this.login }
+        />
         </ScrollView>
       </View>
     );
@@ -25,6 +56,112 @@ export default class LinksScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 50
+  },
+  welcome: {
+    flex: 3,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "stretch",
+    backgroundColor: "#fff",
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3
+      },
+      android: {
+        elevation: 20
+      }
+    })
+  },
+  headline: {
+    textAlign: "center",
+    fontSize: 25
+  },
+  center: {
+    textAlign: "center"
+  },
+  orange: {
+    color: "#ec8074"
+  },
+  blue: {
+    color: "#167990"
+  },
+  inputBox: {
+    flexDirection: "row"
+  },
+  buttonRow: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "stretch"
+  },
+  input: {
+    margin: 5,
+    width: 120,
+    height: 40,
+    textAlign: "center",
+    borderColor: "#ec8074",
+    borderWidth: 1
+  },
+  submitButton: {
+    backgroundColor: "#ec8074",
+    padding: 10,
+    margin: 15,
+    height: 40
+  },
+  submitButtonText: {
+    textAlign: "center",
+    color: "white"
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: "rgba(0,0,0,0.4)",
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: "center"
+  },
+  contentContainer: {
+    paddingTop: 30
+  },
+  welcomeContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10
+  },
+  getStartedContainer: {
+    alignItems: "center",
+    marginHorizontal: 50
+  },
+  homeScreenFilename: {
+    marginVertical: 7
+  },
+  codeHighlightText: {
+    color: "rgba(96,100,109, 0.8)"
+  },
+  codeHighlightContainer: {
+    backgroundColor: "rgba(0,0,0,0.05)",
+    borderRadius: 3,
+    paddingHorizontal: 4
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center"
+  },
   tabBarInfoContainer: {
     position: "absolute",
     top: 20,
@@ -45,9 +182,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#fbfbfb",
     paddingVertical: 20
   },
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#fff"
+  tabBarInfoText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
+  },
+  navigationFilename: {
+    marginTop: 5
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: "center"
+  },
+  helpLink: {
+    paddingVertical: 15
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: "#2e78b7"
   }
 });
